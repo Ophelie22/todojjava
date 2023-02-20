3d739ff6fbbe563c847dcb8db951d7dd022675ec
Juliette Vila <contactjuliettevila@gmail.com>
2023-02-20 20:48:13 +0100

    ajout edition

--- src/index.js
+++ src/index.js
@@ -9,11 +9,14 @@ const input = document.querySelector("form > input");
 const todos = [
   {
     text: "je suis une todo",
-    done: false
+    done: false,
+    //ous ajoutons une propriété editMode sur nos todos :
+    editMode: true
   },
   {
     text: "faire du javascript",
-    done: true
+    done: true,
+    editMode: false
   }
 ];
 //Nous allons ensuite créer un écouteur et 
@@ -28,10 +31,17 @@ form.addEventListener("submit", event => {
 //immédiatement la méthode preventDefault().
 //puis on recupere la valeur de ,notre champ grace a la propriete value
 
-
+//Nous allons modifier cette fonction pour créer un élément différent
+// suivant la valeur d'une propriété editMode que nous allons ajouter sur nos todos.
+//Si la propriété vaut true nous créons un élément todo éditable,
+ //sinon nous créons un élément todo classique :
 const displayTodo = () => {
   const todosNode = todos.map((todo, index) => {
+    if (todo.editMode) {
+      return createTodoEditElement(todo, index);
+    } else {
     return createTodoElement(todo, index);
+    }
   });
   ul.innerHTML = "";
   ul.append(...todosNode);
@@ -45,9 +55,15 @@ const createTodoElement = (todo, index) => {
   buttonDelete.innerHTML = "Supprimer";
   //Ensuite, nous ajoutons un écouteur pour l'événement click et nous créons une fonction fléchée 
   //que nous utilisons comme gestionnaire d'événement.
+  const buttonEdit = document.createElement("button");
+  buttonEdit.innerHTML = "Edit";
   buttonDelete.addEventListener("click", event => {
     event.stopPropagation();
     deleteTodo(index);
+  });
+  buttonEdit.addEventListener("click", event => {
+    event.stopPropagation();
+    toggleEditMode(index);
   });
   li.innerHTML = `
     <span class="todo ${todo.done ? "done" : ""}"></span>
@@ -55,13 +71,33 @@ const createTodoElement = (todo, index) => {
   `;
   //Ensuite, nous ajours simplement le bouton
   // comme enfant de l'élément de liste li.
-  li.appendChild(buttonDelete);
   li.addEventListener("click", event => {
     toggleTodo(index);
   });
+  li.append(buttonEdit, buttonDelete);
   return li;
 };
 
+//Il reste à créer la fonction permettant de générer les todos en mode édition :
+const createTodoEditElement = (todo, index) => {
+  const li = document.createElement("li");
+  const input = document.createElement("input");
+  input.type = "text";
+  input.value = todo.text;
+  const buttonSave = document.createElement("button");
+  buttonSave.innerHTML = "Save";
+  const buttonCancel = document.createElement("button");
+  buttonCancel.innerHTML = "Cancel";
+  buttonCancel.addEventListener("click", event => {
+    event.stopPropagation();
+    toggleEditMode(index);
+  });
+  buttonSave.addEventListener("click", event => {
+    editTodo(index, input);
+  });
+  li.append(input, buttonCancel, buttonSave);
+  return li;
+};
 //Ensuite, elles utilise la méthode push() pour ajouter une nouvelle todo à la liste.
 //La todo est un objet avec une propriété text qui a pour valeur l'argument text passé, nous utilisons donc le raccourci syntaxique que nous avons appris, et une propriété done qui a la valeur false.
 //Enfin, nous appelons la méthode displayTodo() qui va recréer les éléments et les afficher sur le DOM
@@ -82,6 +118,16 @@ const deleteTodo = index => {
 };
 const toggleTodo = index => {
   todos[index].done = !todos[index].done;
+  displayTodo();
+};
+const toggleEditMode = index => {
+  todos[index].editMode = !todos[index].editMode;
+  displayTodo();
+};
+const editTodo = (index, input) => {
+  const value = input.value;
+  todos[index].text = value;
+  todos[index].editMode = false;
   displayTodo();
 };
 displayTodo();
